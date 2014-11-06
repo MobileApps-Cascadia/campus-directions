c76fe221c2f42c2fd2181b1ff1e795c18d6dcb77
Lap To <lap_to@yahool.com>
2014-11-06 03:07:08 -0800

    new change.

--- zxing-test/gen/com/google/zxing/client/android/BuildConfig.java
+++ zxing-test/gen/com/google/zxing/client/android/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.laptogithubtestapp;
+package com.google.zxing.client.android;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
